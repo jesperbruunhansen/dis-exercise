42a4c9562b047fca95b51058124cb965cb815aeb
Jesper Bruun Hansen <jesper@mentalact.dk>
2015-09-22 10:13:54 +0200

    updated git ignore

---
+++ main/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Created by jesperbruun on 21/09/15.
+ */
+public class HelloWorld {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+
+}
+
+
--- untitled/src/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by jesperbruun on 21/09/15.
- */
-public class HelloWorld {
-}
